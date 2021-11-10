7cf810e56bfc0a02e6670ff5672a4ad0dcc8fb37
Filippo Bristot <filippo.bristot@hotmail.it>
2021-11-10 17:27:10 +0100

    feat(color-scheme): add prefers color scheme functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
+const bodyEl = document.getElementsByTagName("body")[0];
+// https://ourcodeworld.com/articles/read/1114/how-to-detect-if-the-user-prefers-a-light-or-dark-color-schema-in-the-browser-with-javascript-and-css
+const userPrefersDark = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
+const userPrefersLight = window.matchMedia && window.matchMedia("(prefers-color-scheme: light)").matches;
 
+if (userPrefersDark || userPrefersLight) {
+  bodyEl.classList.add("prefers-color-scheme");
+}
