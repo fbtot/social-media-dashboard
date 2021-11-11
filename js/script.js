6787cea02049b409367e37051a46b1c17415b026
Filippo Bristot <filippo.bristot@hotmail.it>
2021-11-11 10:44:21 +0100

    refactor(js): remove useless comments and add some spaces

--- js/script.js
+++ js/script.js
@@ -9,10 +9,6 @@ const themesObj = {
   "theme-toggle--center": "prefers-color-scheme",
 };
 
-// https://ourcodeworld.com/articles/read/1114/how-to-detect-if-the-user-prefers-a-light-or-dark-color-schema-in-the-browser-with-javascript-and-css
-const userPrefersDark = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
-const userPrefersLight = window.matchMedia && window.matchMedia("(prefers-color-scheme: light)").matches;
-
 function updateTheme(el) {
   for (className in themesObj) {
     bodyEl.classList.remove(themesObj[className]);
@@ -20,7 +16,9 @@ function updateTheme(el) {
 
   bodyEl.classList.add(themesObj[el.id]);
 }
+
 updateTheme(themeToggleCenter);
+
 toggleButtons.forEach((el) => {
   el.addEventListener("click", function () {
     updateTheme(el);
